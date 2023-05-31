7f4976ca77f2c8dadd80d98cbba669bb85cfc698
John Doe <johndoe@example.com01>
2023-05-31 18:29:58 -0500

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '2023-Spring-Angular';
+  title = 'Jonathon 2023 - Spring Angular';
+  welcomeMessage = 'Well, hello there!'
 }
