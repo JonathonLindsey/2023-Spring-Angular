0e5dd6d75dc646a684c72ca4b13ed79ed07cb6db
John Doe <johndoe@example.com01>
2023-05-31 20:09:16 -0500

    8

---
+++ src/app/mock-vehicles.ts
@@ -0,0 +1,22 @@
+import { Vehicle } from "./vehicle";
+
+export const VEHICLES: Vehicle[] = [
+    {
+        id: '6d70ccbe-0010-11ee-be56-0242ac120002',
+        make: 'Honda',
+        model: 'S2000',
+        year: 2003,  
+    },
+    {
+        id: '38df9528-0017-11ee-be56-0242ac120002',
+        make: 'Ford',
+        model: 'Raptor',
+        year: 2023, 
+    },
+    {
+        id: '2bf1e456-0017-11ee-be56-0242ac120002',
+        make: 'Subaru',
+        model: 'WRX',
+        year: 2015,   
+    }
+];
--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Vehicle } from '../vehicle';
+import { VEHICLES } from '../mock-vehicles';
 
 @Component({
   selector: 'app-vehicles',
@@ -7,10 +8,5 @@ import { Vehicle } from '../vehicle';
   styleUrls: ['./vehicles.component.css']
 })
 export class VehiclesComponent {
-  vehicle: Vehicle = {
-    id: '6d70ccbe-0010-11ee-be56-0242ac120002',
-    make: 'Honda',
-    model: 'S2000',
-    year: 2003,
-  };
+  vehicles = VEHICLES
 }
