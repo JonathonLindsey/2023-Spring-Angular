607e11c019c3501a837237b284aeef1e8416f209
John Doe <johndoe@example.com01>
2023-05-31 19:14:21 -0500

    4

--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Vehicle } from '../vehicle';
 
 @Component({
   selector: 'app-vehicles',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./vehicles.component.css']
 })
 export class VehiclesComponent {
-  make = 'Honda'; 
+  vehicle: Vehicle = {
+    id: '6d70ccbe-0010-11ee-be56-0242ac120002',
+    make: 'Honda',
+    model: 'S2000',
+    year: 2003,
+    color: 'green'
+  };
 }
